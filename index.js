63aa6a9d000e5ae816e84a63bcc90ea3fe0dafba
Jason Mulligan <jamullig@adobe.com>
2017-06-06 09:36:25 -0400

    Fleshing out `nClark` class

--- index.js
+++ index.js
@@ -4,28 +4,63 @@
   const fetch = global.fetch || require('node-fetch'),
     host = 'https://westus.api.cognitive.microsoft.com';
 
+  function validate (arg, type, not) {
+    return typeof arg === type && arg !== not;
+  }
+
   class NClark {
-    constructor (id, key) {
+    constructor (id, key, verbose) {
       this.id = id;
       this.key = key;
       this.urls = {
-        predict: String.raw`${host}/luis/v2.0/apps/${this.id}?subscription-key=${this.key}&q=%s&verbose=true`,
-        reply: String.raw`${host}/luis/v2.0/apps/${this.id}?subscription-key=${this.id}&q=%s&contextid=%s&verbose=true`
+        predict: String.raw`${host}/luis/v2.0/apps/${this.id}?subscription-key=${this.key}&q=%s&verbose=${verbose}`,
+        reply: String.raw`${host}/luis/v2.0/apps/${this.id}?subscription-key=${this.id}&q=%s&contextid=%c&verbose=${verbose}`
       };
+    }
+
+    fetch (url) {
+      let ok, text;
+
+      return fetch(url).then(res => {
+        ok = res.ok;
+        text = res.statusText;
+
+        return res.json();
+      }).then(arg => {
+        if (!ok) {
+          throw new Error(text);
+        }
+
+        return arg;
+      });
+    }
+
+    predict (text) {
+      return this.fetch(this.urls.predict.replace('%s', encodeURIComponent(text)));
+    }
+
+    reply (text, context, set = '') {
+      let url = this.urls.predict.replace('%s', encodeURIComponent(text)).replace('%c', encodeURIComponent(context));
+
+      if (validate(set, 'string', '')) {
+        url += '&forceset=' + encodeURIComponent(set);
+      }
+
+      return this.fetch(url);
     }
   }
 
-  function validate (arg, type, not) {
-    return typeof arg !== type || arg === not;
-  }
-
-  function factory (id = '', key = '') {
+  function factory (id = '', key = '', verbose = true) {
     if (!validate(id, 'string', '')) {
       throw new TypeError('"id" is invalid');
     }
 
     if (!validate(key, 'string', '')) {
       throw new TypeError('"key" is invalid');
+    }
+
+    if (!validate(verbose, 'boolean', null)) {
+      throw new TypeError('"verbose" is invalid');
     }
 
     return new NClark(id, key);
